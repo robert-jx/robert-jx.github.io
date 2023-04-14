3209b8f518e9cec622e04caa54b829d8287ca1e0
robert-jx <robertjc_c@163.com>
2023-04-14 09:27:00 +0800

    update projects

--- data/meta/projects.ts
+++ data/meta/projects.ts
@@ -1,5 +1,9 @@
 
 import { StaticImageData } from 'next/image';
+import onlineRetailers from '@/assets/images/projects/online-retailers.png'
+import echarts from '@/assets/images/projects/echarts.png'
+import mdn from '@/assets/images/projects/mdn.png'
+import vite from '@/assets/images/projects/vite.jpg'
 
 export interface Project {
   name: string;
@@ -9,16 +13,33 @@ export interface Project {
 }
 
 export const projects: Project[] = [
+
+  {
+    name: 'translate-content',
+    description:
+      '参与 MDN 文档的本地化，包括翻译并向 MDN 社区提供建设意见',
+    link: 'https://github.com/robert-jx/translated-content',
+    logo: mdn
+  },
   {
     name: 'online-retailers',
     description: '电商后台系统demo开发',
     link: 'https://github.com/robert-jx/online-retailers',
+    logo: onlineRetailers
   },
   {
     name: 'echarts-tool',
     description:
       '基于echarts的二次封装demo，使用vue3+typescript+vite',
     link: 'https://github.com/robert-jx/echarts-tool',
+    logo: echarts
+  },
+  {
+    name: 'vite-demo',
+    description:
+      '提供使用vite创建的vue3模板',
+    link: 'https://github.com/robert-jx/vite-demo',
+    logo: vite
   },
   {
     name: 'dragging-demo',
@@ -37,18 +58,6 @@ export const projects: Project[] = [
     description:
       '基于vue2实现的时间滑动组件demo',
     link: 'https://github.com/robert-jx/time-axis',
-  },
-  {
-    name: 'vite-demo',
-    description:
-      '提供使用vite创建的vue3模板',
-    link: 'https://github.com/robert-jx/vite-demo',
-  },
-  {
-    name: 'translate-content',
-    description:
-      '参与 MDN 文档的本地化，包括翻译并向 MDN 社区提供建设意见',
-    link: 'https://github.com/robert-jx/translated-content',
   },
 
 ];
